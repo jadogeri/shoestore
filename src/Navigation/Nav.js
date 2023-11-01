730cffe873a29a6db471e66847c4dbf082d99440
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2023-11-01 07:45:55 -0500

    added new files

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './NavStyles'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='red_text'>
+      Nav
+    </div>
+  )
+}
+
+export default Nav;
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+      <Text>Card</Text>
+    </div>
+  )
+}
